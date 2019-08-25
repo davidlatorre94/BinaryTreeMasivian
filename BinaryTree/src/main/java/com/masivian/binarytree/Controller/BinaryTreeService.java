43d8928e6bc5bf8f06f6b4c2878ff8f87ca18dd4
davidlatorre94 <davidlatorre94@hotmail.com>
2019-08-24 23:15:52 -0500

    Modificaciòn archivo POM con las dependencias del servicio Rest.

--- BinaryTree/src/main/java/com/masivian/binarytree/Controller/BinaryTreeService.java
+++ BinaryTree/src/main/java/com/masivian/binarytree/Controller/BinaryTreeService.java
@@ -5,8 +5,22 @@ package com.masivian.binarytree.Controller;
  *
  * @author David
  */
-public class BinaryTreeService {
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+@Path("/MasivianBinaryTree")
+public class BinaryTreeService {
+    
+    @GET
+    @Produces("application/json")
+    public Response prueba(){
+        return Response.status(200).entity("Pr")
+    }
+            
 
     
 }
